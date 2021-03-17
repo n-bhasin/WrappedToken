349aafe64a982f10e9958e3b90f6b5eea5ed6942
Bhasin Neeraj <bhasineeraj@MacBook-Pro.local>
2021-03-17 00:33:27 -0400

    Updates

---
+++ precache-manifest.dc8f5558450500580b372ac8477258b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cde7e314490b001549639bb0863258e",
+    "url": "/WrappedToken/client/index.html"
+  },
+  {
+    "revision": "b03521ed246b7ac8b65b",
+    "url": "/WrappedToken/client/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b92c6a574dc3ce949fb5",
+    "url": "/WrappedToken/client/static/js/2.d1e188ff.chunk.js"
+  },
+  {
+    "revision": "b03521ed246b7ac8b65b",
+    "url": "/WrappedToken/client/static/js/main.9501bf8a.chunk.js"
+  },
+  {
+    "revision": "98fef35ea68a508d7f96",
+    "url": "/WrappedToken/client/static/js/runtime-main.3ca3370e.js"
+  }
+]);
